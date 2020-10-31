819139751c3293269f495efce5170c3597b56983
Muhammad Wara Bin Wafa Khan <Khanwara1@gmail.com>
2020-10-31 18:16:59 +0500

    4

--- frontEnd/app.js
+++ frontEnd/app.js
@@ -1,11 +1,1 @@
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-console.log("hello")
-console.log('hello World');
-=======
-console.log("hello react native ")
->>>>>>> origin/master
-=======
-console.log("hello react native ")
-console.log('testing')
->>>>>>> Stashed changes
+console.log("Muhammad ")
