21b5fb1ac6a3acb84e47deaf1abe30b256f06113
Muhammad Wara Bin Wafa Khan <Khanwara1@gmail.com>
2020-10-31 18:14:35 +0500

    g

--- frontEnd/app.js
+++ frontEnd/app.js
@@ -1,6 +1,11 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 console.log("hello")
 console.log('hello World');
 =======
 console.log("hello react native ")
 >>>>>>> origin/master
+=======
+console.log("hello react native ")
+console.log('testing')
+>>>>>>> Stashed changes
